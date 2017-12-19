6087b172ec48b4e22e71220b69ab85b85e919eb0
Gerrit Kaul <webmaster@gerritk.net>
2017-12-19 18:30:34 +0100

    do not displaying maneuver columns without any maneuver

--- src/app/components/maneuver/maneuver.component.ts
+++ src/app/components/maneuver/maneuver.component.ts
@@ -27,8 +27,9 @@ export class ManeuverComponent implements OnChanges {
     if (this.ship != null) {
       this.shipManeuvers = _.cloneDeep(this.ship.maneuvers);
 
+      // ignoring first maneuver speeds without maneuvers (typically speed 0)
       for (let i = 0; i < this.shipManeuvers.length; i++) {
-        const maneuverCount = _.reduce(this.shipManeuvers[i], (sum: number, n) => sum + n);
+        const maneuverCount = _.reduce(this.shipManeuvers[i], (sum, n) => sum + n, 0);
 
         if (maneuverCount === 0) {
           this.speedOffset++;
@@ -38,6 +39,18 @@ export class ManeuverComponent implements OnChanges {
       }
 
       this.shipManeuvers.splice(0, this.speedOffset);
+
+      // ignoring maneuver types without any maneuver set
+      for (let i = 0; i < this.shipManeuvers[0].length; i++) {
+        const maneuversOfType = _.reduce(this.shipManeuvers, (sum, n) => sum + n[i], 0);
+
+        if (maneuversOfType === 0) {
+          for (let n = 0; n < this.shipManeuvers.length; n++) {
+            this.shipManeuvers[n][i] = -1;
+          }
+        }
+      }
+
       this.shipManeuvers = _.reverse(this.shipManeuvers);
     } else {
       this.shipManeuvers = [];
