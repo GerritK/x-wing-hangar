d174f64a30820228f827cf037ff17532ed0c20d2
Gerrit Kaul <webmaster@gerritk.net>
2018-01-07 11:43:51 +0100

    added tantive iv expansion pack

--- src/app/models/ship.model.ts
+++ src/app/models/ship.model.ts
@@ -5,21 +5,25 @@ import {ShipSize} from '../enums/ship-size.enum';
 
 export class Ship {
   public id: string;
+  public canonicalId?: string;
   public factions: Faction[] = [];
   public stats: ShipStats;
   public actions: Action[] = [];
   public maneuvers: Array<number[]> = [];
   public size: ShipSize;
+  public epicPoints?: number;
 
   public static fromData(data): Ship {
     const result = new Ship();
 
     result.id = data.id;
+    result.canonicalId = data.canonicalId;
     result.factions = Faction.parseArray(data.factions);
     result.stats = ShipStats.fromData(data.stats);
     result.actions = Action.parseArray(data.actions);
     result.maneuvers = data.maneuvers;
     result.size = ShipSize.parse(data.size);
+    result.epicPoints = data.epicPoints;
 
     return result;
   }
